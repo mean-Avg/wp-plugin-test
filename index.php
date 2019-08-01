d476b952b8863f8e6c68e970326f921863f95c58
avg <1994.avg@gmail.com>
2019-08-02 05:22:24 +0530

    db test7

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
 
 echo "ello <br>";
 
-   $dbhost = 'www.remotemysql.com:3306';
+   $dbhost = 'remotemysql.com:3306';
    $dbuser = 'FtHAFS7GzI';
    $dbpass = 'oY1TS7K5n4';
    
    $conn = mysql_connect($dbhost, $dbuser, $dbpass);
    
    if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
+      die('Could not connect: '.mysql_error());
    }
    
 //    $sql = 'SELECT pid, company_name, project_type, budget FROM free_tier_list';
