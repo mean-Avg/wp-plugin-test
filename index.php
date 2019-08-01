30900195a8fc1860bb744c0dfc9287e69982ee9d
avg <1994.avg@gmail.com>
2019-08-02 03:51:45 +0530

    db test4

--- index.php
+++ index.php
@@ -17,22 +17,22 @@
       die('Could not connect: ' . mysql_error());
    }
    
-//    $sql = 'SELECT pid, company_name, project_type, year, budget FROM free_tier_list';
-//    mysql_select_db('FtHAFS7GzI');
-//    $retval = mysql_query( $sql, $conn );
+   $sql = 'SELECT pid, company_name, project_type, year, budget FROM free_tier_list';
+   mysql_select_db('FtHAFS7GzI');
+   $retval = mysql_query( $sql, $conn );
    
-//    if(! $retval ) {
-//       die('Could not get data: ' . mysql_error());
-//    }
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
    
-//    while($row = mysql_fetch_assoc($retval)) {
-//       echo "PID :{$row['pid']}  <br> ".
-//          "COMPANY : {$row['company_name']} <br> ".
-//          "PROJECT TYPE : {$row['project_type']} <br> ".
-//          "YEAR : {$row['year']} <br> ".
-//          "BUDGET : {$row['budget']} <br> ".
-//          "--------------------------------<br>";
-//    }
+   while($row = mysql_fetch_assoc($retval)) {
+      echo "PID :{$row['pid']}  <br> ".
+         "COMPANY : {$row['company_name']} <br> ".
+         "PROJECT TYPE : {$row['project_type']} <br> ".
+         "YEAR : {$row['year']} <br> ".
+         "BUDGET : {$row['budget']} <br> ".
+         "--------------------------------<br>";
+   }
    
    echo "Fetched data successfully\n";
    
