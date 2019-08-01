f2d6ff9e2753fba70c933be71fe730327c83256d
avg <1994.avg@gmail.com>
2019-08-02 05:00:26 +0530

    db test7

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>Test</h1>
 
 <?php
 
@@ -19,23 +19,22 @@ echo "ello <br>";
    if(! $conn ) {
       die('Could not connect: ' . mysql_error());
    }
-   else echo "connected <br>";
    
-   $sql = 'SELECT pid, company_name, project_type, budget FROM free_tier_list';
-   mysql_select_db('FtHAFS7GzI');
-   $retval = mysql_query( $sql, $conn );
+//    $sql = 'SELECT pid, company_name, project_type, budget FROM free_tier_list';
+//    mysql_select_db('FtHAFS7GzI');
+//    $retval = mysql_query( $sql, $conn );
    
-   if(! $retval ) {
-      die('Could not get data: ' . mysql_error());
-   }else echo "data retrieved <br>";
+//    if(! $retval ) {
+//       die('Could not get data: ' . mysql_error());
+//    }
    
-   while($row = mysql_fetch_assoc($retval)) {
-      echo "PID :{$row['pid']}  <br> ".
-         "COMPANY : {$row['company_name']} <br> ".
-         "PROJECT TYPE : {$row['project_type']} <br> ".
-         "BUDGET : {$row['budget']} <br> ".
-         "--------------------------------<br>";
-   }
+//    while($row = mysql_fetch_assoc($retval)) {
+//       echo "PID :{$row['pid']}  <br> ".
+//          "COMPANY : {$row['company_name']} <br> ".
+//          "PROJECT TYPE : {$row['project_type']} <br> ".
+//          "BUDGET : {$row['budget']} <br> ".
+//          "--------------------------------<br>";
+//    }
    
    echo "Fetched data successfully\n";
    
